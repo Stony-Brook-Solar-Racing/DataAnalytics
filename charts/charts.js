c0565f16706b255a870bece687133fd4e94f5d49
Angela <atom@stuy.edu>
2020-02-09 18:30:18 -0500

    fix charts

--- charts/charts.js
+++ charts/charts.js
@@ -1,11 +1,9 @@
 
 var ctx = document.getElementById('myChart').getContext('2d');
-var updateButton = document.getElementById('update');
 
 
 var xlabels = ["Time"]
 var ylabels = [0]
-
 
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -27,16 +25,23 @@ var chart = new Chart(ctx, {
 });
 
 var randomNumbers = function() {
-    return parseInt(Math.random(0,500) * 100)
+    return parseInt(Math.random(0,500) * 100);
 }
 
 
 var update = function() {
-    chart.data.labels.push(randomNumbers());
-    chart.data.datasets[0].data.push(randomNumbers());
+    var x  = randomNumbers();
+    var y = randomNumbers();
+    xlabels.push(x);
+    ylabels.push(y);
+    /*chart.data.labels.push(x);
+    chart.data.datasets[0].data.push(y);*/
+    if (xlabels.length > 10) {
+        xlabels.shift();
+        ylabels.shift();
+    };
     chart.update();
 }
 
-updateButton.addEventListener('click', update)
 
 setInterval(update, 3000)
