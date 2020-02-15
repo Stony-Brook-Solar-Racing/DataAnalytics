b389ba8624f37eb88615c4ae288a38e356e92f60
ivlin <ivlin112@gmail.com>
2020-02-15 17:01:35 -0500

    Added readarduino.py

---
+++ readarduino.py
@@ -0,0 +1,35 @@
+import serial
+ser = serial.Serial('/dev/ttyACM0',9600)
+
+from datetime import datetime
+import requests
+import random 
+
+url = "http://localhost:5000/submitForm"
+
+def parse_data(line):
+    print("> parsing line :"+line)
+    try:
+        a= {"velocity": int(line), "ISOString": datetime.now().isoformat()}
+        print a
+        return a
+    except:
+        print "fail to parse"
+        return None
+
+if __name__ == "__main__":
+    while 1:
+        if (ser.in_waiting > 0):
+            line = ser.readline()
+            data = parse_data(line)
+            if data:
+                print "data:", data
+                try:
+                    r = requests.post(url=url, data = data, timeout=1)
+                    print data
+                except requests.exceptions.ReadTimeout:
+                    pass
+                print "end send"
+            else:
+                print "invalid line"
+
